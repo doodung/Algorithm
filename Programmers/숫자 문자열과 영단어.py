12d0ea254ced9bbca1f400c13046264e181d9904
SooJeong Lee <leesujung0917@gmail.com>
2021-11-24 22:25:44 +0900

    숫자 문자열과 영단어
    2021 카카오 채용연계형 인턴십

---
+++ Programmers/숫자 문자열과 영단어.py
@@ -0,0 +1,11 @@
+import re
+
+def solution(s):
+    dic = {'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7',
+           'eight': '8', 'nine': '9'}
+
+    for i, j in dic.items():
+        if i in s:
+            s = re.sub(i, j, s)
+
+    return int(s)
