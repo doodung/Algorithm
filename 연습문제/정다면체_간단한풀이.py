b2aae6eb670de56ff1073dd00b84c83046e75820
SooJeong Lee <leesujung0917@gmail.com>
2022-02-06 23:49:32 +0900

    Update 정다면체_간단한풀이.py

--- 연습문제/정다면체_간단한풀이.py
+++ 연습문제/정다면체_간단한풀이.py
@@ -2,12 +2,3 @@ import sys
 
 # sys.stdin = open("input.txt", "rt")
 N, M = map(int, input().split())
-cnt = [0] * (N + M + 1)
-
-for i in range(1, N + 1):
-    for j in range(1, M + 1):
-        cnt[i + j] += 1
-
-for i in range(len(cnt)):
-    if cnt[i] == max(cnt):
-        print(i, end=' ')
