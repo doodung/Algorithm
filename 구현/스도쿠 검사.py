717f6da72ce7ccc6881467043dd3d1ae7329e8cd
SooJeong Lee <leesujung0917@gmail.com>
2022-02-20 18:00:41 +0900

    Create 스도쿠 검사.py

---
+++ 구현/스도쿠 검사.py
@@ -0,0 +1,32 @@
+import sys
+
+#sys.stdin = open("input.txt", "r")
+
+
+def check(a):
+    # 행, 열 체크
+    for i in range(9):
+        ch1 = [0] * 10
+        ch2 = [0] * 10
+        for j in range(9):
+            ch1[a[i][j]] = 1
+            ch2[a[j][i]] = 1
+        if sum(ch1) != 9 or sum(ch2) != 9:
+            return False
+    # 3*3 체크
+    for i in range(3):
+        for j in range(3):
+            ch3 = [0] * 10
+            for k in range(3):
+                for s in range(3):
+                    ch3[a[i * 3 + k][j * 3 + s]] = 1
+            if sum(ch3) != 9:
+                return False
+    return True
+
+
+a = [list(map(int, input().split())) for _ in range(9)]
+if check(a):
+    print("YES")
+else:
+    print("NO")
