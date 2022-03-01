be609970b15487dfe627c11fafb59c33cf90bb86
SooJeong Lee <leesujung0917@gmail.com>
2022-03-01 15:46:35 +0900

    Create 약수의 개수와 덧셈 (쉬운 풀이).py

---
+++ 연습문제/약수의 개수와 덧셈 (쉬운 풀이).py
@@ -0,0 +1,8 @@
+def solution(left, right):
+    answer = 0
+    for i in range(left,right+1):
+        if int(i**0.5)==i**0.5:
+            answer -= i
+        else:
+            answer += i
+    return answer
