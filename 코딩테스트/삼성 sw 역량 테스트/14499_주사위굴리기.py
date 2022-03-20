a846162a68c40e1691d210d7ee8931ca8e70a387
SooJeong Lee <leesujung0917@gmail.com>
2022-03-20 14:01:49 +0900

    Update 14499_주사위굴리기.py

--- 코딩테스트/삼성 sw 역량 테스트/14499_주사위굴리기.py
+++ 코딩테스트/삼성 sw 역량 테스트/14499_주사위굴리기.py
@@ -6,13 +6,15 @@ board = [list(map(int, input().split())) for i in range(N)]
 move = list(map(int, input().split()))
 dx = [0, 0, -1, 1]
 dy = [1, -1, 0, 0]
+# 가장 처음 주사위값은 0
+# 윗면, 동쪽, 서쪽, 북쪽, 남쪽, 바닥면 순
 dice = [0, 0, 0, 0, 0, 0]
 nx, ny = x, y
 
 
 def turn(dir):
     a, b, c, d, e, f = dice[0], dice[1], dice[2], dice[3], dice[4], dice[5]
-    # 동 1 서 2 북 3 남 4
+    # 회전 : 동 1 서 2 북 3 남 4
     if dir == 1:
         dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = d, b, a, f, e, c
     elif dir == 2:
@@ -24,16 +26,22 @@ def turn(dir):
 
 
 for dir in move:
+    # 움직이는 nx,ny 좌표를 구함
     nx += dx[dir - 1]
     ny += dy[dir - 1]
+    # 맵보다 커지지 않도록
     if nx < 0 or nx >= N or ny < 0 or ny >= M:
+        #원래 값으로 복구
         nx -= dx[dir - 1]
         ny -= dy[dir - 1]
         continue
     turn(dir)
+    # 칸이 0 이라면 주사위 바닥면을 복사
     if board[nx][ny] == 0:
         board[nx][ny] = dice[-1]
+    # 지도 칸에 값이 있다면 바닥에 칸의 값을 넣고, 칸은 0으로
     else:
         dice[-1] = board[nx][ny]
         board[nx][ny] = 0
+    # 결과 : 상단에 쓰여 있는 값
     print(dice[0])
