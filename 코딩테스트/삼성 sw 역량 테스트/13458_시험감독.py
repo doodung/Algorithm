f2d5b62daad934f7975d3950c87186693b051640
SooJeong Lee <leesujung0917@gmail.com>
2022-03-20 14:21:49 +0900

    Update 13458_시험감독.py

--- 코딩테스트/삼성 sw 역량 테스트/13458_시험감독.py
+++ 코딩테스트/삼성 sw 역량 테스트/13458_시험감독.py
@@ -4,13 +4,17 @@
 N = int(input())
 A = list(map(int, input().split()))
 B, C = map(int, input().split())
+# 총감독, 부감독
 manager, assi = 0, 0
 
 for i in range(N):
+    # 총감독은 무조건 1명
     A[i] -= B
     manager += 1
     if A[i] >= 0:
+        # 부감독은 한명당 C명 가능, 부감독은 1명 이상 가능
         assi += A[i] // C
+        # 위의 계산 후에도 응시자수가 남아있을 수 있으므로 나머지가 0이 아니면 부감독 1명이 더필요함
         if A[i] % C != 0:
             assi += 1
 
