4ab2261748a16f6d3c59f7421a6536ab41250952
SooJeong Lee <leesujung0917@gmail.com>
2022-04-17 21:45:21 +0900

    Create 짝지어 제거하기.py

---
+++ 연습문제/짝지어 제거하기.py
@@ -0,0 +1,13 @@
+def solution(s):
+    stack = []
+    for i in range(len(s)):
+        if not stack:
+            stack.append(s[i])
+        else:
+            if s[i] == stack[-1]:
+                stack.pop()
+            else:
+                stack.append(s[i])
+
+    if stack : return 0          
+    else : return 1
