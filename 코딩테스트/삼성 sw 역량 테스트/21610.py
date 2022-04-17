e494a98b37033f8f44821b70b06087528a1ec0ab
SooJeong Lee <leesujung0917@gmail.com>
2022-04-17 21:46:45 +0900

    Update 21610.py

--- 코딩테스트/삼성 sw 역량 테스트/21610.py
+++ 코딩테스트/삼성 sw 역량 테스트/21610.py
@@ -1,5 +1,4 @@
 # 마법사 상어와 비바라기 - BOJ 21610
-# 구현
 N, M = map(int, input().split())
 water = [list(map(int, input().split())) for _ in range(N)]
 clouds = [[N - 2, 0], [N - 2, 1], [N - 1, 0], [N - 1, 1]]
@@ -14,14 +13,14 @@ for i in range(M):
 
 for k in range(M):
     next_clouds = []
-    # step1 구름 이동 : %N을 해서 범위 넘어서도 가능하게 해야함
+    # 1 구름 이동 : %N을 해서 범위 넘어서도 가능하게 해야함
     for cloud in clouds:
         nx = (N + cloud[0] + dx[move[k][0]] * move[k][1]) % N
         ny = (N + cloud[1] + dy[move[k][0]] * move[k][1]) % N
         next_clouds.append([nx, ny])
-    visited = [[False] * N for _ in range(N)]
 
-    # step2 비가 내려 물의 양 1 증가
+    visited = [[False] * N for _ in range(N)]
+    # 2 비가 내려 물의 양 1 증가
     for cloud in next_clouds:
         water[cloud[0]][cloud[1]] += 1
         visited[cloud[0]][cloud[1]] = True
@@ -37,12 +36,12 @@ for k in range(M):
         for i in range(4):
             nx = cloud[0] + cx[i]
             ny = cloud[1] + cy[i]
-            # step4 물복사버그 마법 : 대각선 방향에 물이 있는 바구니 수만큼 바구니 물 양 증가
+            # 4 물복사버그 마법 : 대각선 방향에 물이 있는 바구니 수만큼 바구니 물 양 증가
             if 0 <= nx < N and 0 <= ny < N and water[nx][ny] >= 1:
                 count += 1
         water[cloud[0]][cloud[1]] += count
 
-    # step5 물의양2 이상인 모든 칸에 구름 생기고 -> 물의 양 2 줄어든다
+    # 5 물의양2 이상인 모든 칸에 구름 생기고 -> 물의 양 2 줄어든다
     for i in range(N):
         for j in range(N):
             if water[i][j] >= 2 and visited[i][j] == False:
