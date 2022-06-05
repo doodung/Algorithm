652b938edead934d40ccf5d671e57afa51027a4c
SooJeong Lee <leesujung0917@gmail.com>
2022-06-05 22:34:13 +0900

    Create 거품정렬.py

---
+++ 정렬/거품정렬.py
@@ -0,0 +1,5 @@
+def bubble_sort(arr):
+    for i in range(1, len(arr)):
+        for j in range(0, len(arr) - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
